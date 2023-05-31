e8fecb18e1de1cab2828cdbd5521ea383133624d
sai4ul <web.saifulislam@gmail.com>
2023-05-31 20:27:18 +0600

    js updated

--- assets/js/mian.js
+++ assets/js/mian.js
@@ -69,37 +69,37 @@
     /*-----------------------------------
         Back To TOP
     -----------------------------------*/
-    // (function(){
-    //     var progressPath = document.querySelector('.progressParent path');
-    //     var pathLength = progressPath.getTotalLength();
-    //     progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-    //     progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-    //     progressPath.style.strokeDashoffset = pathLength;
-    //     progressPath.getBoundingClientRect();
-    //     progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
-    //     var updateProgress = function () {
-    //     var scroll = $(window).scrollTop();
-    //     var height = $(document).height() - $(window).height();
-    //     var progress = pathLength - (scroll * pathLength / height);
-    //     progressPath.style.strokeDashoffset = progress;
-    //     }
-    //     updateProgress();
-    //     $(window).scroll(updateProgress);	
-    //     var offset = 50;
-    //     var duration = 550;
-    //     jQuery(window).on('scroll', function() {
-    //     if (jQuery(this).scrollTop() > offset) {
-    //         jQuery('.progressParent').addClass('rn-backto-top-active');
-    //     } else {
-    //         jQuery('.progressParent').removeClass('rn-backto-top-active');
-    //     }
-    //     });				
-    //     jQuery('.progressParent').on('click', function(event) {
-    //     event.preventDefault();
-    //     jQuery('html, body').animate({scrollTop: 0}, duration);
-    //     return false;
-    //     })
-    // })();
+    (function(){
+        var progressPath = document.querySelector('.progressParent path');
+        var pathLength = progressPath.getTotalLength();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+        progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+        progressPath.style.strokeDashoffset = pathLength;
+        progressPath.getBoundingClientRect();
+        progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';		
+        var updateProgress = function () {
+        var scroll = $(window).scrollTop();
+        var height = $(document).height() - $(window).height();
+        var progress = pathLength - (scroll * pathLength / height);
+        progressPath.style.strokeDashoffset = progress;
+        }
+        updateProgress();
+        $(window).scroll(updateProgress);	
+        var offset = 50;
+        var duration = 550;
+        jQuery(window).on('scroll', function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.progressParent').addClass('rn-backto-top-active');
+        } else {
+            jQuery('.progressParent').removeClass('rn-backto-top-active');
+        }
+        });				
+        jQuery('.progressParent').on('click', function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+        })
+    })();
 
     /*-----------------------------------
         Mouse Cursor
