ce4077d644f93866af82cf43940c973433bf22ed
sai4ul <web.saifulislam@gmail.com>
2023-06-26 13:06:17 +0600

    mobile menu fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,25 +78,6 @@
         })
     })();
 
-    /*-----------------------------------
-        Mouse Cursor
-    -----------------------------------*/
-    var myCursor = $('.mouseCursor');
-    if (myCursor.length) {
-        if ($('body')) {
-            const e = document.querySelector('.cursorInner'),
-                t = document.querySelector('.cursorOuter');
-            let n, i = 0,
-                o = !1;
-            window.onmousemove = function (s) {
-                o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
-            }, $('body').on("mouseenter", "a, .cursor-pointer", function () {
-                e.classList.add('cursor-hover'), t.classList.add('cursor-hover')
-            }), $('body').on("mouseleave", "a, .cursor-pointer", function () {
-                $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove('cursor-hover'), t.classList.remove('cursor-hover'))
-            }), e.style.visibility = "visible", t.style.visibility = "visible"
-        }
-    }
     
     /*-----------------------------------
         Slick Nav
@@ -119,10 +100,6 @@
     /*-----------------------------------
        Write Your Any JS/Jquery Syntax
     -----------------------------------*/
-
-
-    
-
 
 
 }(jQuery));
